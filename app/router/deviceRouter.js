74a72abea17cc9eb8544622b88cfea4d9650f3be
phamducdat <phamducdat2402@gmail.com>
2021-12-18 19:22:22 +0700

    Tạo câu lệnh chạy giả lập

--- app/router/deviceRouter.js
+++ app/router/deviceRouter.js
@@ -4,7 +4,7 @@ const deviceRouter = express.Router();
 const deviceController = require('../controller/deviceController');
 const auth = require("../middleware/auth")
 
-deviceRouter.get("/", deviceController.getAllDevice)
+deviceRouter.get("/",auth, deviceController.getAllDevice)
 deviceRouter.get("/:deviceId", auth, deviceController.getDeviceByDeviceId)
 deviceRouter.put("/:deviceId", auth, deviceController.updateDeviceByDeviceId);
 deviceRouter.post("/:deviceId", auth, deviceController.updateStateHistoryByDeviceId);
